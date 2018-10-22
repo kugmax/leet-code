007b3dde23757fa19787967e75e515cb0f9c3c4e
maxim.kugatov <maximkugatov@gmail.com>
2018-10-22 12:56:57 +0300

    junit 5 is suck(((

--- src/test/java/com/kugmax/learn/algorithms/sort/TestInsertionSort.java
+++ src/test/java/com/kugmax/learn/algorithms/sort/TestInsertionSort.java
@@ -10,7 +10,7 @@ public class TestInsertionSort {
     public void test() {
         InsertionSort algorithm = new InsertionSort();
 
-        int[] result = algorithm.sort(new int[]{5, 2, 4, 6, 1, 3});
-        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, result);
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6}, algorithm.sort(new int[]{5, 2, 4, 6, 1, 3}));
+        assertArrayEquals(new int[]{26, 31, 41, 41, 58, 59}, algorithm.sort(new int[]{31, 41, 59, 26, 41, 58}));
     }
 }
