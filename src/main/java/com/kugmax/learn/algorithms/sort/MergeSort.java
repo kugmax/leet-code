c3525d48c8f9762872cc59c824460bb0b03169ab
maxim.kugatov <maximkugatov@gmail.com>
2018-10-27 18:15:57 +0300

    Changes to be committed: 	new file:   src/main/java/com/kugmax/learn/algorithms/sort/MergeSort.java

---
+++ src/main/java/com/kugmax/learn/algorithms/sort/MergeSort.java
@@ -0,0 +1,13 @@
+package com.kugmax.learn.algorithms.sort;
+
+public class MergeSort implements SortAlgorithm {
+    @Override
+    public int[] sort(int[] a) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] sort(int[] a, boolean reverse) {
+        return new int[0];
+    }
+}
